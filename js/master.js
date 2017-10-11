9aa3e19b3c2e3c8f050d184da3294c8cd986ce26
Daniel Lomas <daniel.lomas@itcuk.com>
2017-10-11 12:35:25 +0100

    JS added to footer and main nav

---
+++ js/master.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function(){
+	var urlLinks = [
+		{
+			page:"Home",
+			link:"index"
+		},
+		{
+			page:"About",
+			link:"about"
+		},
+		{
+			page:"Office Cleaning",
+			link:"office-cleaning"
+		},
+		{
+			page:"Domestic Cleaning",
+			link:"domestic-cleaning"
+		},
+		{
+			page:"Work for us",
+			link:"work-for-us"
+		},
+		{
+			page:"Contact",
+			link:"contact"
+		}
+	];
+
+	var pageExt = ".html";
+	var navigation = document.querySelector(".navigation");
+	var footerNavigation = document.querySelector(".footer-navigation");
+	navigation.innerHTML = "";
+	footerNavigation.innerHTML = "";
+
+	for(i=0;i<urlLinks.length;i++){
+		navigation.innerHTML += "<li><a href="+urlLinks[i].link+".html>"+urlLinks[i].page+"</a></li>";
+		footerNavigation.innerHTML += "<li><a href="+urlLinks[i].link+pageExt+">"+urlLinks[i].page+"</a></li>";
+	}
+}, false);
